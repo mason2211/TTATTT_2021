45060b31cc5f3ae82c59f258c54efda56d38dcdc
Pham Minh Son <76507402+mason2211@users.noreply.github.com>
2022-12-24 21:07:33 +0700

    Update Cau6.c

--- Cau6.c
+++ Cau6.c
@@ -2,28 +2,20 @@
 cả các ước của số kia (trừ chính số đó) và ngược lại. Viết chương trình tìm hai số dạng này nhỏ
 hơn N (với N nhập vào từ bàn phím).*/
 #include <stdio.h>
-int thanThiet(long long int a,long long int b){
-    long long int d1=0, d2=0;
-    for(long long int i=1,j=1;i<=a/2,j<=b/2;i++,j++){
+long long int tongUoc(long long int a){
+    long long int d=1;
+    for(long long int i=2;i<=a/2;i++)
         if(a%i==0)
-            d1+=i;
-        if(b%j==0)
-            d2+=j;
-    }    
-    if(d1==b && d2==a)
-            return 1;
-    return 0;
+            d+=i;
+    return d;
 }
 int main(){
-    long long int n, d=0;
+    long long int n,j;
     scanf("%lld",&n);
-    for(long long int i=1;i<n;i++){
-        for(long long int j=i+1;j<(2*i<n?2*i:n);j++)
-            if(thanThiet(i,j)==1)
-                printf("%lld va %lld thoa man\n",i,j);   
-        if(i==n-1)
-            printf("Ket thuc");
+    for(long long int i=1;i<=n;i++){
+        j = tongUoc(i);
+        if(tongUoc(j)==i && j>i)  //(điều kiện a>i đề loại trường hợp đảo vị trí của j với i)
+            printf("%lld %lld\n",i,j);
     }
-
     return 0;
 }
