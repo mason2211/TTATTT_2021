1e62a030a7ed74718e437cdaee5127274162dd42
Pham Minh Son <76507402+mason2211@users.noreply.github.com>
2022-12-27 23:30:23 +0700

    Update Cau2.c

--- Cau2.c
+++ Cau2.c
@@ -1,27 +1,23 @@
 /*Câu 2. Viết chương trình tìm các số nguyên tố có N chữ số với N nhập từ bàn phím và 2 ≤ N ≤ 10.*/
 #include <stdio.h>
 #include <math.h>
-int isPrime(long long int n){
-    if (n <= 1)
-        return 0;
-    if (n == 2 || n == 3)
-        return 1;
-    if (n % 2 == 0 || n % 3 == 0)
-        return 0;
-    for (long int i = 5; i <= sqrt(n); i+=6)
-        if (n % i == 0 || n % (i + 2) == 0)
-            return 0;
-    return 1;
-}
 int main(){
-    long long int n;
-    scanf("%lld",&n);
+    int n;
+    scanf("%d",&n);
     while (n<2 || n>10){
         printf("Nhap lai N thoa man 2 <= N <= 10: ");
         scanf("%lld",&n);
     }
-    for(long long int i=pow(10,n-1);i<=pow(10,n)-1;i++)
-        if(isPrime(i)==1)
-            printf("%lld ",i);
+    int k2 = pow(10,n), k1 = pow(10,n-1);
+    int check[k2+1]; //Sàng NT
+    for(int i = 2 ; i < k2 ; i++)
+        check[i]=1;
+    for(int i = 2 ; i < k2 ; i++) 
+        if(check[i]==1)
+            for(int j = 2*i; j<k2; j+=i)
+                check[j]=0;
+    for(int i = k1; i < k2; i++)
+        if(check[i]==1)
+            printf("%d ",i);  
     return 0;
 }
