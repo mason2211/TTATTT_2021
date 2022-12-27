80aa55b86befe0ef30e79bdf72b49a2213a1092d
Pham Minh Son <76507402+mason2211@users.noreply.github.com>
2022-12-27 23:33:52 +0700

    Update Cau3.c

--- Cau3.c
+++ Cau3.c
@@ -9,26 +9,21 @@ p=1+2+3+4+6+8+12+24=60 và s=8 trong đó có 2 ước nguyên tố là {2,3} do
 Và từ đó: N+p+s-q-k = 24+60+8-5-2=85;*/
 #include <stdio.h>
 #include <math.h>
-int isPrime(long long int n){
-    if (n <= 1)
-        return 0;
-    if (n == 2 || n == 3)
-        return 1;
-    if (n % 2 == 0 || n % 3 == 0)
-        return 0;
-    for (long int i = 5; i <= sqrt(n); i+=6)
-        if (n % i == 0 || n % (i + 2) == 0)
-            return 0;
-    return 1;
-}
 int main(){
     long long int n, k = 0, q = 0, p = 0, s = 0;
     scanf("%lld", &n);
+    long long int check[n+1]; //Sàng NT
+    for(long long int i = 2 ; i <= n ; i++)
+        check[i]=1;
+    for(long long int i = 2 ; i <= n ; i++) 
+        if(check[i]==1)
+            for(long long int j = 2*i; j<=n; j+=i)
+                check[j]=0;
     for (long long int i = 1; i <= n; i++){
         if (n % i == 0){
             s += 1;
             p += i;
-            if (isPrime(i) == 1){
+            if (check[i] == 1){
                 k += 1;
                 q += i;
             }
