64ad2a8e9d3e23ab9620160f9ee31593d5dc75b1
Pham Minh Son <76507402+mason2211@users.noreply.github.com>
2022-12-27 23:58:57 +0700

    Update Cau14.c

--- Cau14.c
+++ Cau14.c
@@ -2,18 +2,6 @@
 ngược lại thì ta được một số là lập phương của một số tự nhiên.*/
 #include <stdio.h>
 #include <math.h>
-int isPrime(long long int n){
-    if (n <= 1)
-        return 0;
-    if (n == 2 || n == 3)
-        return 1;
-    if (n % 2 == 0 || n % 3 == 0)
-        return 0;
-    for (long int i = 5; i <= sqrt(n); i+=6)
-        if (n % i == 0 || n % (i + 2) == 0)
-            return 0;
-    return 1;
-}
 int daoNguoc(int n){
     int n1 = 0;
     while (n > 0){
@@ -30,10 +18,16 @@ int isCubeNumber(int n){
     return 0;
 }
 int main(){
-    for (int i = 101; i <= 1000; i++){
-        if (isPrime(i) == 1)
+    int check[1000+1]; //Sàng NT
+    for(int i = 2 ; i <= 1000 ; i++)
+        check[i]=1;
+    for(int i = 2 ; i <= 1000 ; i++) 
+        if(check[i]==1)
+            for(int j = 2*i; j<=1000; j+=i)
+                check[j]=0;
+    for (int i = 101; i < 1000; i++)
+        if (check[i] == 1)
             if(isCubeNumber(daoNguoc(i))==1)
                 printf("So do la: %d ", i);
-            }
     return 0;
 }
