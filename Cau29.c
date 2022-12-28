6f59a0792130f5fe2812c738cec6a0d286232723
Pham Minh Son <76507402+mason2211@users.noreply.github.com>
2022-12-28 22:29:12 +0700

    Update Cau29.c

--- Cau29.c
+++ Cau29.c
@@ -10,18 +10,15 @@ long long int gcd(long long int a, long long int b){
         return b;
     return gcd(b, a % b);
 }
-long long int mod(long long int a, long long int b, long long int n){
-    return ((a%n)*(b%n))%n;
-}
 long long int nhanBinhPhuongCoLap(long long int a, long long int k, long long int n){
     long long int b = 1, A = a;
     if (k % 2 == 1)
         b = a;
     k/=2;
     while (k > 0){
-        A = mod(A,A,n);
+        A = (A*A)%n;
         if (k % 2 == 1)
-            b = mod(b,A,n);
+            b = (b*A)%n;
         k/=2;
     }
     return b;
