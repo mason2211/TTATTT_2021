a97c7b1e3dcbeefe532ec054c39ad20d81b3a549
Pham Minh Son <76507402+mason2211@users.noreply.github.com>
2022-12-29 17:34:32 +0700

    Update Cau42.c

--- Cau42.c
+++ Cau42.c
@@ -4,21 +4,30 @@ liệt kê những số a thoả mãn: a^p mod q là số nguyên tố.*/
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-long long int mod(long long int a, long long int b, long long int c) {
-    return ((a % c) * (b % c)) % c;
-}
 long long int nhanBinhPhuongCoLap(long long int a, long long int k, long long int n) {
     long long int b = 1, A = a;
     if (k % 2 == 1)
         b = a;
     k /= 2;
     while (k > 0) {
-        A = mod(A, A, n);
+        A = (A*A)%n;
         if (k % 2 == 1)
-            b = mod(b, A, n);
+            b = (b*A)%n;
         k /= 2;
     }
     return b;
+}
+int isPrime(long long int n){
+    if (n <= 1)
+        return 0;
+    if (n == 2 || n == 3)
+        return 1;
+    if (n % 2 == 0 || n % 3 == 0)
+        return 0;
+    for (long long int i = 5; i <= sqrt(n); i+=6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return 0;
+    return 1;
 }
 int Miller_Rabin(long long int a, long long int n) {
     long long int x = n - 1, s=0, r;
@@ -29,9 +38,9 @@ int Miller_Rabin(long long int a, long long int n) {
     r = x;
     long long int y = nhanBinhPhuongCoLap(a, r, n);
     if (y != 1 && y != n - 1) {
-        int j = 1;
+        long long int j = 1;
         while (j <= s - 1 && y != n - 1) {
-            y = mod(y, y, n);
+            y = (y*y)%n;
             if (y == 1) 
                 return 0;
             j++;
@@ -39,18 +48,6 @@ int Miller_Rabin(long long int a, long long int n) {
         if (y != n - 1) 
             return 0;
     }
-    return 1;
-}
-int isPrime(long long int n){
-    if (n <= 1)
-        return 0;
-    if (n == 2 || n == 3)
-        return 1;
-    if (n % 2 == 0 || n % 3 == 0)
-        return 0;
-    for (long int i = 5; i <= sqrt(n); i+=6)
-        if (n % i == 0 || n % (i + 2) == 0)
-            return 0;
     return 1;
 }
 int testMiller_Rabin(long long int n, long long int t){
@@ -62,54 +59,42 @@ int testMiller_Rabin(long long int n, long long int t){
             return 0;
         else 
             check = 1;
-        a++;
     }
     if(check==1)
         return 1;
 }
 long long int RANDOMSEARCH(long long int k, long long int t){
-    long long int n, snt, B;
-    long long int tmp = pow(2,k)-1;
-    b1:while(1){     
-        n = rand()%(tmp+1);
-        B=rand();
-        while (1){
-            snt = rand()%(B+1);
-            if(isPrime(snt)!=1)
-                snt = rand()%(B+1);
-            else break;
-        }    
-        if(n%snt==0)
-            goto b1;
-        if(testMiller_Rabin(n,t)==1)
-            break;      
-    }
-    return n;
+    long long int n, snt, B, tmp = pow(2,k)-1;
+    b1:     
+    n = rand()%(tmp+1);
+    B=rand();
+    while (1){
+        snt = rand()%(B+1);
+        if(isPrime(snt)!=1)
+             snt = rand()%(B+1);
+        else break;
+    }    
+    if(n%snt==0) goto b1;
+    if(testMiller_Rabin(n,t)==1)
+        return n;
+    else goto b1;      
 }
 int main() {
     srand((int)time(0));
-    long long int k = rand()%(10+1);
+    long long int k = rand()%(9+1);
     long long int t = rand()%(20+1); 
-    long long int p = RANDOMSEARCH(k,t);
-    long long int q = RANDOMSEARCH(k,t);
-    while(p==q){
-        p= RANDOMSEARCH(k,t);
-        q= RANDOMSEARCH(k,t);
-    }
+    long long int p= RANDOMSEARCH(k,t);
+    long long int q= RANDOMSEARCH(k,t);
     printf("p = %lld, q = %lld ",p,q);
-    int check=0;
-    long long int a=1;
-    while (1){
+    long long int a=2, check=0;
+    while (a<100){
         long long int kq=nhanBinhPhuongCoLap(a,p,q); 
         if(isPrime(kq)==1){
             check=1;
-            printf("\nkq = %lld, a = %lld ",kq,a);
+            printf("\nkq = %3lld, a = %3lld ",kq,a);
         }
-        a++;
-        if(a==100)
-            break;    
+        a++;   
     }     
-    if(check==0)
-        printf("Khong co so a thoa man!");        
+    if(check==0) printf("Khong co so a thoa man!");        
     return 0;
 }
