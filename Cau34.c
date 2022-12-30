c61f6ad62e79ee008172834127ea24cbb0d8da7e
Pham Minh Son <76507402+mason2211@users.noreply.github.com>
2022-12-30 12:43:14 +0700

    Update Cau34.c

--- Cau34.c
+++ Cau34.c
@@ -2,42 +2,40 @@
 cho kết quả kiểm tra sai.*/
 #include <stdio.h>
 #include <math.h>
-#include <stdlib.h>
-#include <time.h>
-int nhanBinhPhuongCoLap(int a, int k, int n){
-    int b=1, A=a;
-    if(k%2==1)
-        b=a;
-    k/=2;
-    while(k%2==0){
+long long int nhanBinhPhuongCoLap(long long int a, long long int k, long long int n) {
+    long long int b = 1, A = a;
+    if (k % 2 == 1)
+        b = a;
+    k /= 2;
+    while (k > 0) {
         A = (A*A)%n;
-        if(k%2==1)
-            b = (A*b)%n;
-        k/=2;
+        if (k % 2 == 1)
+            b = (b*A)%n;
+        k /= 2;
     }
     return b;
 }
-int main(){
-    //a^(n-1) mod n
-    int n, t, a=2, check=0;
-    srand((int)time(0));
-    n = rand();
-    t = 500+rand()%(1000+1-500);
-    printf("Kiem tra so nguyen n=%d trong %d lan:\n",n,t);
-    while(t-->0){
-        a = 2+rand()%(n+1-4);
-        int r = nhanBinhPhuongCoLap(a,n-1,n);
-        if(r!=1){
-            //printf("Co so a=%d: Hop so\n",a);
+int Fermat(long long int a,long long int n) {
+    long long int r = nhanBinhPhuongCoLap(a, n - 1, n);
+    if (r!=1)
+        return 0;
+    return 1;
+}
+int main() {
+    long long int n, t, a=2, check = 1;
+    scanf("%lld%lld", &n, &t);
+    while (t-->0) {
+        if (Fermat(a, n)!= 1){
+            printf("Co so a=%d: Hop so\n",a);
+            check = 0;
             break;
-        } 
-        else {
-            //printf("Co so a=%d: Nguyen to\n",a);
-            check = 1;
         }
+        else
+            printf("Co so a=%d: Nguyen to\n",a);  
+        a++;
     }
-    if(check==1)
-        printf("%d co the la nguyen to",n);
-    else printf("%d la hop so",n);
+    if (check == 1)
+        printf("%lld co the la nguyen to", n);
+    else printf("%lld la hop so", n);
     return 0;
 }
