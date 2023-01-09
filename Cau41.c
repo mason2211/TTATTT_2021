74a879ca77732795a926520ea670fad25a605ab8
Pham Minh Son <76507402+mason2211@users.noreply.github.com>
2023-01-09 23:43:43 +0700

    Update Cau41.c

--- Cau41.c
+++ Cau41.c
@@ -4,44 +4,47 @@ a^k mod n có phải là một số nguyên tố hay không (sử dụng thuật
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-int isPrime(long long int n){
-    if(n<2)
-        return 0;
-    if(n==2 || n==3)
-        return 1;
-    if(n%2==0 || n%3==0)
-        return 0;
-    for(long long int i=0;i<=sqrt(n);i+=6)
-        if(n%i==0 || n%(i+2)==0)
-            return 0;
-    return 1;
-}
-long long int mod(long long int a, long long int b, long long int c){
-    return ((a%c)*(b%c))%c;
-}
 long long int nhanBinhPhuongCoLap(long long int a, long long int k, long long int n){
     long long int b = 1, A = a;
     if (k % 2 == 1)
         b = a;
     k/=2;
     while (k > 0){
-        A = mod(A,A,n);
+        A = (A*A)%n;
         if (k % 2 == 1)
-            b = mod(b,A,n);
+            b = (b*A)%n;
         k/=2;
     }
     return b;
 }
+int isPrime(long long int n){
+    if (n <= 1)
+        return 0;
+    if (n == 2 || n == 3)
+        return 1;
+    if (n % 2 == 0 || n % 3 == 0)
+        return 0;
+    for (long int i = 5; i <= sqrt(n); i+=6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return 0;
+    return 1;
+}
 int main(){
     srand((int)time(0));
-    long long int a = rand()%(1000+1);
-    long long int k = rand()%(1000+1);
-    long long int n = rand()%(1000+1);
-    while (a>=n){
-        a = rand()%(1000+1);
+    // long long int a = rand()%(1000+1);
+    // long long int k = rand()%(1000+1);
+    // long long int n = rand()%(1000+1);
+    long long int a,k,n;
+    scanf("%lld%lld%lld",&a,&k,&n);
+    while (a>=n || a<=0 || a>=1000){
+        //a = rand()%(1000+1);
+        printf("Nhap lai a thoa man 0<a,k<n<1000: ");
+        scanf("%lld",&a);
     }
-    while (k>=n){
-        k = rand()%(1000+1);
+    while (k>=n || k<=0 || k>=1000){
+        //k = rand()%(1000+1);
+        printf("Nhap lai a thoa man 0<a,k<n<1000: ");
+        scanf("%lld",&k);
     }
     printf("a = %lld, k = %lld, n = %lld\n",a,k,n);
     long long int x = nhanBinhPhuongCoLap(a,k,n);
