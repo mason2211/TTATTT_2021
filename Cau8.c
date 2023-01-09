32a425194d3d0c409b0c0a424b52d631c69e7fd7
Pham Minh Son <76507402+mason2211@users.noreply.github.com>
2023-01-09 23:32:30 +0700

    Update Cau8.c

--- Cau8.c
+++ Cau8.c
@@ -9,10 +9,17 @@ long long int demUoc(long long int n){
     return d;
 }
 int main(){
-    long long int n;
+    long long int n,check=0;
     scanf("%lld",&n);
+    while(n<=0){
+        printf("Nhap n thoa man de bai: ");
+        scanf("%lld",&n);
+    }
     for(long long int i=2;i<=n;i++)
-        if(demUoc(i)==3)
+        if(demUoc(i)==3){
+            check=1;
             printf("%lld ",i);
+        }
+    if(!check) printf("Khong co so T-prime < %lld",n);
     return 0;
 }
